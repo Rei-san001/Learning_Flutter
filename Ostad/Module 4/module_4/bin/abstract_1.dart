c687e0e3e7f89b8b038832e1e40a783d4103175c
179292160 <Rei-san001@users.noreply.github.com>
2025-03-31 02:31:46 +0600

    updated

--- Ostad/Module 4/module_4/bin/abstract_1.dart
+++ Ostad/Module 4/module_4/bin/abstract_1.dart
@@ -1,8 +1,12 @@
-class Student {
-  //method
-  void attendClass() {
-    print("He Joined The Class");
-  }
+import 'dart:collection';
+
+abstract class Student {
+  //abstract method
+  //if we want to make a method abstract then we have to remove the body
+  void attendClass();
+  // {
+  //   print("He Joined The Class");
+  // }
 
   void examSubmit() {}
 }
@@ -12,10 +16,57 @@ class Person extends Student {
 
   //constructor of the Person (child class)
   Person(this.name);
+
+  @override
+  void attendClass() {
+    print("$name joined class");
+  }
+}
+
+//when using implement we have override every method in parent class
+class BusinessMan implements Student {
+  String name;
+  BusinessMan(this.name);
+
+  @override
+  void attendClass() {
+    print("$name businessman joined class");
+  }
+
+  @override
+  void examSubmit() {
+    print("$name attended exam");
+  }
 }
 
 main() {
+  // Student s1 = Student();
+
   Person p1 = Person('REI');
   print(p1.name);
   p1.attendClass();
+
+  BusinessMan b1 = BusinessMan("Natsume");
+  b1.examSubmit();
+
+  //polymorphism
+
+  Student gojo = Person('Gojo');
+  Student madara = BusinessMan('Madara');
+
+  gojo.attendClass();
+  madara.attendClass();
+
+  print(gojo.runtimeType);
+  print(madara.runtimeType);
+
+  //true/false value
+  print(gojo is Person);
+  print(madara is Person);
+
+  print(gojo is BusinessMan);
+  print(madara is BusinessMan);
+
+  print(gojo is Student);
+  print(madara is Student);
 }

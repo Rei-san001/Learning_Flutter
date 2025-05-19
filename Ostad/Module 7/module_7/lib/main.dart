9fd09646caa1ab6d4826429755ba42d8fa0fac1d
179292160 <Rei-san001@users.noreply.github.com>
2025-05-20 00:48:29 +0600

    test

--- Ostad/Module 7/module_7/lib/main.dart
+++ Ostad/Module 7/module_7/lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp()
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }

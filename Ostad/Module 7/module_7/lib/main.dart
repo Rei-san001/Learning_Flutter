98a53445898f08ebb78f28505aa51574ca6f56b4
179292160 <Rei-san001@users.noreply.github.com>
2025-05-20 11:41:42 +0600

    testing

--- Ostad/Module 7/module_7/lib/main.dart
+++ Ostad/Module 7/module_7/lib/main.dart
@@ -10,6 +10,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Hello World', 
+        home: Home(),
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.cyan,
+      ),
+    );
   }
 }
